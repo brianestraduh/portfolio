b2ca693c84d4f76bac47c4596bcd600073867d5a
Brian Estrada <bestr2015@gmail.com>
2023-11-20 09:53:28 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//toggle theme
+
+document.querySelector("#toggle-theme").addEventListener("click", () => {
+  document.documentElement.classList.toggle("light-theme");
+});
+
+const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+
+if (prefersLight) {
+  document.documentElement.classList.add("light-theme");
+}
+
+//arrow scroll down
+document.querySelector("#scroll-down").addEventListener("click", () => {
+  window.scrollTo({
+      top: document.querySelector("#about-me").offsetTop - 20,
+  });
+});
